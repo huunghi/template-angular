f2fa546e79ef3792ae906fb4cf48b56eb62d504b
Huu Nghi <firendshiptran@gmail.com>
2020-06-04 10:49:03 +0700

    change app id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { SidebarModule } from 'primeng/sidebar';
     NotFoundComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: 'angular-template' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
